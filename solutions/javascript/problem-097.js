89f429718f92f0a434a10c1be22eb2d59848300e
myTerminal <team.terminal@gmail.com>
2020-02-06 07:35:19 -0600

    import JS solutions 089, 092, 096, 097, 099, 102, 112

---
+++ solutions/javascript/problem-097.js
@@ -0,0 +1,30 @@
+﻿/* global require module */
+
+const {
+    addBigNumbers,
+    multiplyBigNumbers
+} = require('./common');
+
+module.exports.run = () => {
+	const power = 7830457;
+
+    const getLastTenDigits = numberInString => {
+	    const len = numberInString.length;
+
+	    return numberInString.substring(len - 10);
+	};
+
+    let init = '2',
+        output;
+
+    for (let i = 1; i <= power - 1; i++) {
+	    init = addBigNumbers(init, init);
+	    init = getLastTenDigits(init);
+    }
+
+    output = multiplyBigNumbers(init, '28433');
+    output = getLastTenDigits(output);
+    output = addBigNumbers(output, '1');
+
+    return output;
+};
---
+++ solutions/javascript/problem-112.js
@@ -0,0 +1,49 @@
+/* global module */
+
+module.exports.run = () => {
+	const targetPercentage = 99;
+
+    const isIncreasingNumber = number => {
+	    const numberInString = number.toString();
+		const digits = numberInString.split('');
+
+		let isNegative;
+
+	    for (let i = 1; i < digits.length && !isNegative; i++) {
+		    isNegative = (+digits[i] - +digits[i - 1]) < 0;
+        }
+
+	    return !isNegative;
+	};
+
+	const isDecreasingNumber = number => {
+	    const numberInString = number.toString();
+		const digits = numberInString.split('');
+
+        let isPositive;
+
+	    for (let i = 1; i < digits.length && !isPositive; i++) {
+		    isPositive = (+digits[i] - +digits[i - 1]) > 0;
+        }
+
+	    return !isPositive;
+	};
+
+    let number,
+	    countOfBouncyNumbers = 0,
+	    currentPercentage;
+
+    for (let i = 100; !number; i++) {
+	    if (!isIncreasingNumber(i) && !isDecreasingNumber(i)) {
+	        countOfBouncyNumbers++;
+        }
+
+	    currentPercentage = (countOfBouncyNumbers * 100) / i;
+
+	    if(currentPercentage == targetPercentage) {
+	        number = i;
+        }
+    }
+
+    return number;
+};
