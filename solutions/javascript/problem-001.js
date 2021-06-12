720b32ac3b827a77b69c133e3f530be189db838d
myTerminal <team.terminal@gmail.com>
2021-06-11 23:46:03 -0500

    simplify JavaScript solution for problem 001

--- solutions/javascript/problem-001.js
+++ solutions/javascript/problem-001.js
@@ -3,5 +3,5 @@
 module.exports.run = () =>
     Array.from(new Array(1000))
         .map((e, i) => i)
-        .filter(i => !(i % 3 && i % 5))
+        .filter(i => !(i % 3) || !(i % 5))
         .reduce((a, c) => a + c, 0);
